04bb906ae155fdb1f290ad256717bdbd67528224
mitchy-dev <mitchy.dev@gmail.com>
2022-10-20 07:05:08 +0900

    ログイン状態に応じてヘッダーの表示を変更する機能を実装

--- header.php
+++ header.php
@@ -2,21 +2,37 @@
 <div class="l-wrapper">
   <header class="l-header">
     <div class="c-header-title">
-      <a href="" class="c-header-title__link">
+      <a href="index.php" class="c-header-title__link">
         海岸ロケ
       </a>
     </div>
     <ul class="c-header-nav">
-      <li class="c-header-nav__item c-button c-button__text">
-        <a>
-          ログイン
-        </a>
-      </li>
-      <li class="c-header-nav__item c-button c-button__secondary">
-        <a>
-          ユーザー登録
-        </a>
-      </li>
+      <?php
+      if (empty($_SESSION['login_limit'])) : ?>
+        <li class="c-header-nav__item c-button c-button__text">
+          <a href="login.php">
+            ログイン
+          </a>
+        </li>
+        <li class="c-header-nav__item c-button c-button__secondary">
+          <a href="signUp.php">
+            ユーザー登録
+          </a>
+        </li>
+      <?php
+      else: ?>
+        <li class="c-header-nav__item c-button c-button__text">
+          <a href="logout.php">
+            ログアウト
+          </a>
+        </li>
+        <li class="c-header-nav__item c-button c-button__secondary">
+          <a href="mypage.php">
+            マイページ
+          </a>
+        </li>
+      <?php
+      endif; ?>
     </ul>
     <div class="c-notification-bar__container">
       <span class="c-notification-bar__message js-flash-message"><?php
