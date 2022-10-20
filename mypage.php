7657a0b41b04c2630b4f506626f229e1dc6dbc92
mitchy-dev <mitchy.dev@gmail.com>
2022-10-20 10:42:05 +0900

    サイドバーを別ファイルに切り出し

--- mypage.php
+++ mypage.php
@@ -15,29 +15,9 @@ require "header.php";
 
 
   <main class="l-main">
-    <section class="l-sidebar__container">
-      <ul class="c-sidebar__list">
-        <li class="c-sidebar__item --active --border-top"><a href="">お気に入り</a></li>
-      </ul>
-      <h3 class="c-sidebar__list-title">海岸の管理</h3>
-      <ul class="c-sidebar__list">
-        <li class="c-sidebar__item"><a href="">登録した海岸</a></li>
-        <li class="c-sidebar__item"><a href="">下書き</a></li>
-        <li class="c-sidebar__item"><a href="">海岸の登録</a></li>
-      </ul>
-      <h3 class="c-sidebar__list-title">事前相談・撮影申請先</h3>
-      <ul class="c-sidebar__list">
-        <li class="c-sidebar__item"><a href="">登録した情報</a></li>
-        <li class="c-sidebar__item"><a href="">事前相談・撮影申請先の登録</a></li>
-      </ul>
-      <h3 class="c-sidebar__list-title">個人設定</h3>
-      <ul class="c-sidebar__list">
-        <li class="c-sidebar__item"><a href="">プロフィール</a></li>
-        <li class="c-sidebar__item"><a href="">パスワード</a></li>
-        <li class="c-sidebar__item"><a href="">メールアドレス</a></li>
-        <li class="c-sidebar__item"><a href="">退会</a></li>
-      </ul>
-    </section>
+    <?php
+    require "sidebar.php"; ?>
+
     <div class="l-main__my-page">
       <h1 class="c-main__title u-text-center"><?php
         echo $pageTitle; ?></h1>
---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<section class="l-sidebar__container">
+  <ul class="c-sidebar__list">
+    <li class="c-sidebar__item --active --border-top"><a href="">お気に入り</a></li>
+  </ul>
+  <h3 class="c-sidebar__list-title">海岸の管理</h3>
+  <ul class="c-sidebar__list">
+    <li class="c-sidebar__item"><a href="">登録した海岸</a></li>
+    <li class="c-sidebar__item"><a href="">下書き</a></li>
+    <li class="c-sidebar__item"><a href="">海岸の登録</a></li>
+  </ul>
+  <h3 class="c-sidebar__list-title">事前相談・撮影申請先</h3>
+  <ul class="c-sidebar__list">
+    <li class="c-sidebar__item"><a href="">登録した情報</a></li>
+    <li class="c-sidebar__item"><a href="">事前相談・撮影申請先の登録</a></li>
+  </ul>
+  <h3 class="c-sidebar__list-title">個人設定</h3>
+  <ul class="c-sidebar__list">
+    <li class="c-sidebar__item"><a href="">プロフィール</a></li>
+    <li class="c-sidebar__item"><a href="">パスワード</a></li>
+    <li class="c-sidebar__item"><a href="">メールアドレス</a></li>
+    <li class="c-sidebar__item"><a href="withdrawal.php">退会</a></li>
+  </ul>
+</section>
