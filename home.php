c465b585be6ff27e0820c31f3bba99df70a46897
mitchy-dev <mitchy.dev@gmail.com>
2022-12-24 05:02:25 +0900

    topページ用にcssファイルを作ることをやめた

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+
+require( 'functions.php' );
+startPageDisplay();
+
+
+endPageDisplay();
+?>
+<?php
+$pageTitle = 'ホーム';
+require "head.php";
+require "header.php";
+?>
+
+<main class="l-main">
+
+</main>
+
+<?php
+require "footer.php"; ?>
