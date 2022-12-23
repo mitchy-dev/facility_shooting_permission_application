ed9122dae5987985e66868f66e24d68d9b75d567
mitchy-dev <mitchy.dev@gmail.com>
2022-12-23 10:56:33 +0900

    style.cssをappディレクトリに移動

--- head.php
+++ head.php
@@ -9,7 +9,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="destyle.css">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="app/style.css">
   <title>
     <?php
     echo $pageTitle . ' | 海岸ロケ'; ?>
