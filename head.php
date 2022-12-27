f9480c19171a53d47a946b64ecc3d70218e5c609
mitchy-dev <mitchy.dev@gmail.com>
2022-12-28 05:41:09 +0900

    OGPを設定

--- head.php
+++ head.php
@@ -1,6 +1,25 @@
 <!doctype html>
 <html lang="ja">
-<head>
+<head prefix="og:http://ogp.me/ns#">
+  <!--  OGP設定  -->
+  /* 共通 */
+  <meta property="og:title" content="ロケに合う海岸が見つかる"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:url" content="https://kaigan-loca.com"/>
+  <meta property="og:image" content="https://kaigan-loca.com/img/ogp.png"/>
+  <meta property="og:image:type" content="image/png"/>
+  <!--  <meta property="og:image:width" content="1200"/>-->
+  <!--  <meta property="og:image:height" content="630"/>-->
+  <meta property="og:site_name" content="海岸ロケ"/>
+  <meta property="og:description" content="ロケ地探しの面倒を減らす。ロケの申請先が分かる。"/>
+  <meta name="thumbnail" content="https://kaigan-loca.com/img/ogp.png"/>
+
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:site" content="@mitchy_dev"/>
+
+  <!--  <meta property="fb:app_id" content="AppID"/>-->
+
+  <!--  サイトの設定  -->
   <meta charset="UTF-8">
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
