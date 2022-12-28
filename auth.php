41e0c16b51ce2cf08c72215f6915549072cdeb5c
mitchy-dev <mitchy.dev@gmail.com>
2022-12-29 06:36:51 +0900

    ログイン時にユーザー登録画面が表示される不具合を解消

--- auth.php
+++ auth.php
@@ -1,24 +1,29 @@
 <?php
 
-if (!empty($_SESSION['login_limit'])) {
-    debug('ログイン済ユーザーです');
-    if (time() < $_SESSION['login_limit']) {
-        debug('ログイン有効期限内です。');
-        $_SESSION['login_limit'] = time() + WEEK;
-        if (basename($_SERVER['PHP_SELF']) === 'login.php') {
-            debug('マイページに遷移します');
-            redirect('mypage.php');
-        }
-    } else {
-        debug('ログイン有効期限が切れています。ログインページに遷移します');
-        if (basename($_SERVER['PHP_SELF']) !== 'login.php') {
-            redirect('login.php');
-        }
-    }
+if ( ! empty( $_SESSION['login_limit'] ) ) {
+	debug( 'ログイン済ユーザーです' );
+	if ( basename( $_SERVER['PHP_SELF'] ) === 'signUp.php' ) {
+		debug( 'マイページに遷移します' );
+		redirect( 'mypage.php' );
+	}
+
+	if ( time() < $_SESSION['login_limit'] ) {
+		debug( 'ログイン有効期限内です。' );
+		$_SESSION['login_limit'] = time() + WEEK;
+		if ( basename( $_SERVER['PHP_SELF'] ) === 'login.php' ) {
+			debug( 'マイページに遷移します' );
+			redirect( 'mypage.php' );
+		}
+	} else {
+		debug( 'ログイン有効期限が切れています。ログインページに遷移します' );
+		if ( basename( $_SERVER['PHP_SELF'] ) !== 'login.php' ) {
+			redirect( 'login.php' );
+		}
+	}
 } else {
-    debug('未ログインユーザーです');
-    if (basename($_SERVER['PHP_SELF']) !== 'login.php') {
-        redirect('login.php');
-    }
+	debug( '未ログインユーザーです' );
+	if ( basename( $_SERVER['PHP_SELF'] ) !== 'login.php' ) {
+		redirect( 'login.php' );
+	}
 }
 
