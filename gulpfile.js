bb2f0e199b6aaec6307ba4a3b4d6d83bfd68bf0b
mitchy-dev <mitchy.dev@gmail.com>
2023-03-31 15:00:16 +0900

    browserifyのタスクの自動化するコードにコメントを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+//JavaScriptの圧縮に必要なパッケージ
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
@@ -6,21 +7,24 @@ const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 
-function browserifyTask() {
+//CSSのビルドに必要なパッケージ
+
+
+function browserifyTask() { //タスクの関数を定義
     return browserify({entries: 'src/js/app.js', debug: true})
         // .transform('babelify', {presets: ['@babel/preset-env']})
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('public/js'));
+        .bundle() //単一のファイルにまとめる
+        .pipe(source('app.js')) //browserifyからgulpのストリームに変換
+        .pipe(buffer()) //gulpストリームをJavaScriptオブジェクトに変換
+        .pipe(sourcemaps.init({loadMaps: true})) //sourceマップを作成
+        .pipe(rename({suffix: '.min'})) //ファイル名に.minを追加
+        .pipe(uglify()) //JavaScriptを圧縮
+        .pipe(sourcemaps.write('.')) //sourceマップを書き出し
+        .pipe(gulp.dest('public/js')); //ファイルに出力
 }
 
 function watch() {
-    gulp.watch('src/js/**/*.js', browserifyTask);
+    gulp.watch('src/js/**/*.js', browserifyTask); //監視対象のパスを指定、変更あれば指定のタスクを実行
 }
 
-exports.default = gulp.series(browserifyTask, watch);
+exports.default = gulp.series(browserifyTask, watch); //デフォルトタスクを定義
